f9420f543e67364645a38a0487a0953245248723
IEKOZIN <1032253543@pfur.ru>
2025-11-16 13:44:45 +0300

    laba 06

--- labs/lab06/report/lab6-4.asm
+++ labs/lab06/report/lab6-4.asm
@@ -1,25 +1,34 @@
 %include 'in_out.asm'
+
 SECTION .data
-msg: DB 'Введите значение переменной х: ',0
+msg: DB 'Введите значение переменной x: ',0
 rem: DB 'Результат: ',0
+
 SECTION .bss
 x: RESB 80
+
 SECTION .text
 GLOBAL _start
+
 _start:
-mov eax, msg
-call sprint
-mov ecx, x
-mov edx, 80
-call sread
-mov eax, x
-call atoi
-mov ebx, 31
-mul ebx
-sub eax, 5
-add eax, 10
-mov edi, eax
-mov eax, rem
-call sprint
-mov eax, edi
-call iprint
+    mov eax, msg
+    call sprint
+    mov ecx, x
+    mov edx, 80
+    call sread
+    mov eax, x
+    call atoi
+
+    sub eax, 1
+    mov ebx, 3
+    mul ebx
+    mov ebx, 4
+    xor edx, edx
+    div ebx
+    add eax, 5
+
+    mov edi, eax
+    mov eax, rem
+    call sprint
+    mov eax, edi
+    call iprint
